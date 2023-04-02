39c1e0749804a28c60e8eeee893774fb169b14d6
Lambels <patrick.arvatu@yahoo.com>
2023-04-02 15:37:58 +0300

    almost finished algorithm

--- src/game.rs
+++ src/game.rs
@@ -1,6 +1,8 @@
-use std::io::Read;
+use std::{io::Read, mem};
 
 use crate::parser::{Command, Parser, ParsingError};
+
+const EMPTY_BOX: u32 = 0;
 
 struct Columns<'a> {
     inner: &'a mut [u32; 16],
@@ -14,7 +16,7 @@ impl Columns<'_> {
 }
 
 impl<'a> Iterator for Columns<'a> {
-    type Item = &'a mut [u32];
+    type Item = &'a mut u32;
 
     fn next(&mut self) -> Option<Self::Item> {
         todo!()
@@ -28,40 +30,83 @@ impl<'a> DoubleEndedIterator for Columns<'a> {
 }
 
 #[derive(Default)]
-struct GameState {
-    game: [u32; 16],
-}
-
-// impl Default for GameState {
-//     fn default() -> Self {
-//         Self { game: [0; 16] }
-//     }
-// }
+struct GameState([u32; 16]);
 
 impl GameState {
     fn shift(&mut self, direction: Command) -> u32 {
         let stack_emitter: Box<dyn DoubleEndedIterator<Item = _>> = match direction {
-            Command::MoveUp | Command::MoveDown => Box::new(Columns::new(&mut self.game, 4)),
-            Command::MoveLeft | Command::MoveRight => Box::new(self.game.chunks_exact_mut(4)),
+            Command::MoveUp | Command::MoveDown => Box::new(Columns::new(&mut self.0, 4)),
+            Command::MoveLeft | Command::MoveRight => {
+                Box::new(self.0.iter_mut())
+            }
             _ => unreachable!(),
         };
 
-        stack_emitter
-            .map(|v| GameState::calculate_stack(v, direction))
-            .sum()
+        // apply mask for inverted iterators.
+        match direction {
+            Command::MoveLeft | Command::MoveDown => {
+                stack_emitter = Box::new(stack_emitter.rev());
+            }
+            _ => {}
+        }
+
+        let mut count = 0;
+        let mut total = 0;
+        
+        // set compare (ideal) reference to the top element of the stack.
+        let mut comp_ref: Option<&mut u32> = Some(stack_emitter.next().expect("The board must have 16 entries"));
+        // free ref is reference to an element on the stack which is always free.
+        let mut free_ref: Option<&mut u32> = None;
+        while let Some(current_ref) = stack_emitter.next() {
+            count += 1;
+            if count % 4 == 0 {
+                comp_ref = Some(current_ref);
+                continue;
+            }
+
+            if *current_ref == EMPTY_BOX && free_ref.is_none() {
+                free_ref = Some(current_ref);
+                continue;
+            }
+
+            GameState::swap_with_fallback(current_ref, comp_ref, free_ref);
+        }
+        
+        total
     }
 
-    fn reset(&mut self) {}
+    fn swap_with_fallback(v: &mut u32, ideal: Option<&mut u32>, fallback: Option<&mut u32>) -> u32 {
+        if let Some(ideal) = ideal {
+            if *v == *ideal {
+                *ideal = *v * 2;
+                v
+            } else if let Some(fallback) = fallback {
+                            
+            } else {
+    
+            }
+        } else if let Some(v) = fallback {
 
-    fn calculate_stack(stack: &mut [u32], direction: Command) -> u32 {
-        todo!()
+        }  
     }
+
+    #[inline]
+    fn reset(&mut self) {
+        self.0.fill(EMPTY_BOX);
+    }
+
+    fn over(&self) -> bool {
+        false
+    }
+
+    fn populate(&mut self) {}
 }
 
 pub struct Game<R> {
     parser: Parser<R>,
 
     game: GameState,
+    score: u32,
 }
 
 impl<R> Game<R> {
@@ -69,20 +114,37 @@ impl<R> Game<R> {
         Game {
             parser,
             game: GameState::default(),
+            score: 0,
         }
     }
 }
 
+pub enum GameExit {
+    ParseError(ParsingError),
+    Quit,
+}
+
+impl From<ParsingError> for GameExit {
+    fn from(value: ParsingError) -> Self {
+        GameExit::ParseError(value)
+    }
+}
+
 impl<R: Read> Game<R> {
-    pub fn game_loop(mut self) -> Result<(), ParsingError> {
+    pub fn game_loop(mut self) -> Result<u32, GameExit> {
         loop {
+            if self.game.over() {
+                return Ok(self.score);
+            }
+            self.game.populate();
+
             match self.parser.parse_command()? {
-                Command::Quit => return Ok(()),
+                Command::Quit => return Err(GameExit::Quit),
                 x @ (Command::MoveLeft
                 | Command::MoveRight
                 | Command::MoveUp
                 | Command::MoveDown) => {
-                    self.game.shift(x);
+                    self.score += self.game.shift(x);
                 }
                 Command::Restart => self.game.reset(),
             };
