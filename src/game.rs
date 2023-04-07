5686c5902f407f1302bcc7c5ab92e06475982a2a
Lambels <patrick.arvatu@yahoo.com>
2023-04-07 20:33:29 +0300

    changed approach (again)

--- src/game.rs
+++ src/game.rs
@@ -1,138 +1,157 @@
-use std::{io::Read, mem, ops::Index, usize};
-use core::{array, slice};
+use std::{collections::VecDeque, io::Read};
 
 use crate::parser::{Command, Parser, ParsingError};
 
-const EMPTY_BOX: usize = 0;
+/// The empty value of a box in the 2048 game data model.
+const EMPTY_BOX: u32 = 0;
+/// The standard board size. 4x4
 const BOARD_SIZE: usize = 16;
+/// The standard board width.
 const BOARD_WIDTH: usize = 4;
 
-/// sdgfsg
-struct Columns {
-    dirty: [u32; BOARD_SIZE],
-    dirty_x: usize,
-    clean_x: usize,
-    wrap: usize,
-}
+type WrapIter<T> = dyn DoubleEndedIterator<Item = WrapStep<T>>;
 
-enum WrapStep {
-    Stepped(usize),
-    Wrapped,
-}
-
-impl Columns {
-    fn new(dirty: [u32; BOARD_SIZE]) {
-
-    }
-
-    // dont return a wraps step because clean pointer will always be behind dirty pointer.
-    fn step_clean(&mut self) -> usize {
-        
-    }
-
-    fn step_dirty(&mut self) -> (WrapStep, WrapStep) {}
-}
-
-impl Iterator for Columns {
-    type Item = (usize, (u32, u32));
-
-    #[inline]
-    fn next(&mut self) -> Option<Self::Item> {
-        let step = self.step_clean();
-        let (dirty1, dirty2) = self.step_dirty();
-
-        match (dirty1, dirty2) {
-            (WrapStep::Wrapped, _) => Some(step, (EMPTY_BOX, EMPTY_BOX)),
-            (WrapStep::Stepped(x), WrapStep::Wrapped) => Some(step, (x/2, x/2)),
-            (WrapStep::Stepped(x), WrapStep::Stepped(y) => Some(step, (x, y)),
+/// SkipWrap is an extension to an iterator which yields WrapSteps of usize which can be indexed in
+/// an array.
+///
+/// The purpose of this trait is to blanket the implementation for skipping the EMPTY_BOX value for
+/// the current column / row in an array provided the wrapping of the indexes.
+trait SkipWrap: DoubleEndedIterator<Item = WrapStep<usize>> {
+    /// wrap_skip_zero will walk the underlaying interator until it wraps. Before wrapping, if it
+    /// encounters any zero values it skips them, advancing the iterator and returns the first non
+    /// zero value BEFORE WRAPPING. If no non zero value is found before wrapping, the first
+    /// wrapped value will be returned.
+    fn wrap_skip_zero<T: Copy + PartialEq + Eq>(
+        &mut self,
+        source: &[T],
+        zero: T,
+    ) -> Option<WrapStep<T>> {
+        loop {
+            match self.next()? {
+                WrapStep::Stepped(index) if source[index] != zero => {
+                    return Some(WrapStep::Stepped(source[index]))
+                }
+                WrapStep::Stepped(_) => {}
+                WrapStep::Wrapped(index) => return Some(WrapStep::Wrapped(source[index])),
+            }
         }
     }
 }
 
-impl DoubleEndedIterator for Columns {
-    #[inline]
-    fn next_back(&mut self) -> Option<Self::Item> {
-        self.0.next_back()
+/// WrapStep represents a step taken in an iterator which might have wrapped.
+#[derive(Debug, PartialEq, Eq)]
+enum WrapStep<T> {
+    /// Represents a step taken by the iterator which hasnt yet wrapped.
+    Stepped(T),
+    /// Represents the first step which wrapped.
+    Wrapped(T),
+}
+
+/// *_pointer keeps row representation.
+/// *_index keeps col representation.
+struct ColumnWrap {
+    front_pointer: usize,
+    prev_front_index: usize,
+
+    back_pointer: usize,
+    prev_back_index: usize,
+
+    wrap: usize,
+}
+
+impl ColumnWrap {
+    fn new(size: usize, wrap: usize) -> Self {
+        assert_eq!(size % wrap, 0);
+        Self {
+            front_pointer: 0,
+            prev_front_index: 0,
+            back_pointer: size - 1,
+            prev_back_index: size - 1,
+            wrap,
+        }
     }
 }
 
-struct Rows();
+impl Iterator for ColumnWrap {
+    type Item = WrapStep<usize>;
 
-#[derive(Default)]
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.front_pointer > self.back_pointer {
+            return None;
+        } else if self.front_pointer == 0 {
+            self.front_pointer += 1;
+            return Some(WrapStep::Stepped(0));
+        }
+
+        let edge = self.front_pointer % self.wrap;
+        self.front_pointer += 1;
+        if edge == 0 {
+            // we wrapped.
+            self.prev_front_index = (self.prev_front_index % self.wrap) + 1;
+            Some(WrapStep::Wrapped(self.prev_front_index))
+        } else {
+            // we didnt wrap.
+            self.prev_front_index += self.wrap;
+            Some(WrapStep::Stepped(self.prev_front_index))
+        }
+    }
+}
+
+impl DoubleEndedIterator for ColumnWrap {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        todo!()
+    }
+}
+
+struct RowWrap {}
+
+impl RowWrap {
+    fn new(size: usize, wrap: usize) -> Self {
+        todo!()
+    }
+}
+
+impl Iterator for RowWrap {
+    type Item = WrapStep<usize>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        todo!()
+    }
+}
+
+impl DoubleEndedIterator for RowWrap {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        todo!()
+    }
+}
+
+#[derive(Default, PartialEq, Eq)]
 struct GameState([u32; BOARD_SIZE]);
 
 impl GameState {
     fn shift(&mut self, direction: Command) -> u32 {
-        let mut stack_emitter: Box<dyn DoubleEndedIterator<Item = _>> = match direction {
-            Command::MoveUp | Command::MoveDown => Box::new(Columns::new()),
-            Command::MoveLeft | Command::MoveRight => Box::new(Rows::new()),
+        let (mut clean_iter, mut dirty_iter): (Box<WrapIter<_>>, Box<WrapIter<_>>) = match direction {
+            Command::MoveUp | Command::MoveDown => (
+                Box::new(ColumnWrap::new(BOARD_SIZE, BOARD_WIDTH)),
+                Box::new(ColumnWrap::new(BOARD_SIZE, BOARD_WIDTH)),
+            ),
+            Command::MoveLeft | Command::MoveRight => (
+                Box::new(RowWrap::new(BOARD_SIZE, BOARD_WIDTH)),
+                Box::new(RowWrap::new(BOARD_SIZE, BOARD_WIDTH)),
+            ),
             _ => unreachable!(),
         };
 
         // apply mask for inverted iterators.
         match direction {
             Command::MoveLeft | Command::MoveDown => {
-                stack_emitter = Box::new(stack_emitter.rev());
+                clean_iter = Box::new(clean_iter.rev());
+                dirty_iter = Box::new(dirty_iter.rev());
             }
             _ => {}
         }
 
-        let mut count = 0;
-        let mut total = 0;
-        
-        // set compare (ideal) reference to the top element of the stack.
-        let mut comp_ref = Some(stack_emitter.next().expect("The board must have 16 entries"));
-        // free ref is reference to an element on the stack which is always free.
-        let mut free_ref = None;
-        comp_ref = comp_ref.and_then(|v| {
-            if *v == EMPTY_BOX {
-                free_ref = Some(v);
-                None
-            } else {
-                Some(v)
-            }
-        });
-
-        while let Some(current_ref) = stack_emitter.next() {
-            count += 1;
-            if count % 4 == 0 {
-                comp_ref = Some(current_ref);
-                continue;
-            }
-
-            if *current_ref == EMPTY_BOX && free_ref.is_none() {
-                free_ref = Some(current_ref);
-                continue;
-            }
-
-            total += GameState::swap_with_fallback(current_ref, &mut comp_ref, &mut free_ref);
-        }
-        
-        total
-    }
-
-    fn swap_with_fallback<'a>(v: &'a mut u32, ideal: &'a mut Option<&'a mut u32>, fallback: &'a mut Option<&'a mut u32>) -> u32 {
-        if let Some(ref mut ideal_ptr) = *ideal {
-            if *v == **ideal_ptr {
-                let new_val = *v * 2;
-                **ideal_ptr = new_val;
-                *v = EMPTY_BOX;
-                if fallback.is_none() {
-                    *fallback = Some(v);
-                }
-                return new_val / 2;
-            } else if let Some(ref mut fallback) = *fallback {
-                mem::swap(*fallback, v);
-                *ideal = Some(fallback);
-                return 0;
-            }
-        } else if let Some(ref mut fallback_ptr) = *fallback {
-            mem::swap(*fallback_ptr, v);
-            *ideal = fallback.take();
-            return 0;
-        }
-
-        unreachable!()
+        todo!()
     }
 
     #[inline]
@@ -141,7 +160,7 @@ impl GameState {
     }
 
     fn over(&self) -> bool {
-        false
+        todo!()
     }
 
     fn populate(&mut self) {}
@@ -149,7 +168,6 @@ impl GameState {
 
 pub struct Game<R> {
     parser: Parser<R>,
-
     game: GameState,
     score: u32,
 }
@@ -195,5 +213,26 @@ impl<R: Read> Game<R> {
                 Command::Restart => self.game.reset(),
             };
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn column_wrap() {
+        let mut column_iter = ColumnWrap::new(9, 3);
+
+        assert_eq!(Some(WrapStep::Stepped(0)), column_iter.next());
+        assert_eq!(Some(WrapStep::Stepped(3)), column_iter.next());
+        assert_eq!(Some(WrapStep::Stepped(6)), column_iter.next());
+        assert_eq!(Some(WrapStep::Wrapped(1)), column_iter.next());
+        assert_eq!(Some(WrapStep::Stepped(4)), column_iter.next());
+        assert_eq!(Some(WrapStep::Stepped(7)), column_iter.next());
+        assert_eq!(Some(WrapStep::Wrapped(2)), column_iter.next());
+        assert_eq!(Some(WrapStep::Stepped(5)), column_iter.next());
+        assert_eq!(Some(WrapStep::Stepped(8)), column_iter.next());
+        assert_eq!(None, column_iter.next());
     }
 }
